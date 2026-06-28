81697810fda2cce907d5af84311ff4abae41cb6a
tauyoung <tauyoung@icloud.com>
2026-06-28 15:30:00 +0800

    animeko@5.7.0

--- Casks/animeko.rb
+++ Casks/animeko.rb
@@ -2,9 +2,9 @@ cask "animeko" do
   arch arm: "aarch64", intel: "x86_64"
   extension = on_arch_conditional arm: "dmg", intel: "zip"
 
-  version "5.6.0"
-  sha256 arm:   "c1c0e23a4d30ddef4aa6b19bee415f81e20ba2b5cbc3a84ea763ced08776f8ff",
-         intel: "9977dc104529845c3c2fe50aed66a2de92449d84c94c375c8505b9a8b6b44119"
+  version "5.7.0"
+  sha256 arm:   "45641da20e284376c2bc805a5b806792f0e7acb0676491bfb31d823f330088de",
+         intel: "18dacf63f03e06cdd2439e65dadc19582e3a1464e2a76b10325398ccd83218ba"
 
   url "https://d2.myani.org/v#{version}/ani-#{version}-macos-#{arch}.#{extension}",
       verified: "d2.myani.org"
