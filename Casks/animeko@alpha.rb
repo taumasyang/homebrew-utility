2a844a67707e4ec03a0448e75f266488e0cb8d4b
tauyoung <tauyoung@icloud.com>
2026-09-05 22:00:00 +0800

    Deprecate the `verified` parameter in the `url` stanza

--- Casks/animeko@alpha.rb
+++ Casks/animeko@alpha.rb
@@ -6,8 +6,7 @@ cask "animeko@alpha" do
   sha256 arm:   "f25de8d929118f91ee7c480a7254f4f3955f347134aced08a14d0cf2d6f29f0c",
          intel: "9cc2f9abd11b402757045cfbb27f3c27cc48bdd32fc16784fab429d50dd8f98b"
 
-  url "https://d2.myani.org/v#{version}/ani-#{version}-macos-#{arch}.#{extension}",
-      verified: "d2.myani.org"
+  url "https://d2.myani.org/v#{version}/ani-#{version}-macos-#{arch}.#{extension}"
   name "Animeko"
   desc "一站式在线弹幕追番平台"
   homepage "https://animeko.org/"
